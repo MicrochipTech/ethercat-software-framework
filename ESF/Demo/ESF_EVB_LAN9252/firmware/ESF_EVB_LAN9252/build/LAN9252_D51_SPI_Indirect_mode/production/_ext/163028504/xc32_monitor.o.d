8b842e8bcca594723c628a0a14f92ab069a397d7
Rengarajan S <Rengarajan.S@microchip.com>
2023-03-03 20:16:04 +0530

    Updated Project files

---
+++ ESF/Demo/ESF_EVB_LAN9252/firmware/ESF_EVB_LAN9252/build/LAN9252_D51_SPI_Indirect_mode/production/_ext/163028504/xc32_monitor.o.d
@@ -0,0 +1,2 @@
+build/LAN9252_D51_SPI_Indirect_mode/production/_ext/163028504/xc32_monitor.o: \
+ ../src/config/default/stdio/xc32_monitor.c
---
+++ ESF/Demo/ESF_EVB_LAN9252/firmware/ESF_EVB_LAN9252/build/LAN9252_D51_SPI_Indirect_mode/production/_ext/1865555181/plib_qspi.o.d
@@ -0,0 +1,2 @@
+build/LAN9252_D51_SPI_Indirect_mode/production/_ext/1865555181/plib_qspi.o: \
+ ../src/config/default/peripheral/qspi/plib_qspi.c
